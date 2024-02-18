f0b1bba95ba7e55c5430fe25c8f0f9816037ee05
Victor Misyutin <misyutinvictor@gmail.com>
2024-02-18 17:20:45 -0600

    added routing and two pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DocsPageComponent } from './pages/docs-page/docs-page.component';
+export const routes: Routes = [
+    {path: '', component: HomePageComponent, title: 'Home Page'},
+    {path: 'docs', component: DocsPageComponent, title: 'Docs Page'}
 
-export const routes: Routes = [];
+];
