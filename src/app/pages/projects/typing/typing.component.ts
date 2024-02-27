d5967040278dee3ca8e2843d4472b012b6157d5b
Victor Misyutin <misyutinvictor@gmail.com>
2024-02-27 02:08:25 -0600

    almost done with typing game

--- src/app/pages/projects/typing/typing.component.ts
+++ src/app/pages/projects/typing/typing.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { generate, count } from "random-words";
-import { Time } from '@angular/common';
+import { generate } from "random-words";
+
 @Component({
   selector: 'app-typing',
   templateUrl: './typing.component.html',
@@ -15,12 +15,66 @@ export class TypingComponent implements OnInit{
   startTime: number = Date.now();
   timer: number = 0;
   timerRef: any;
+  timed: boolean = true;
+  timeLength: number = 30;
+  WPM: number = 0.0;
+  accuracy: number = 0.0;
+
   ngOnInit(): void {
-    this.generateText(this.numWords);
+    this.setWords(25);
   }
   
+  setWords(words: number){
+    clearInterval(this.timerRef);
+    this.timed = false;
+    this.numWords = words;
+    document.getElementById('10-words')?.classList.remove('active-select');
+    document.getElementById('25-words')?.classList.remove('active-select');
+    document.getElementById('50-words')?.classList.remove('active-select');
+    document.getElementById('100-words')?.classList.remove('active-select');
+
+    document.getElementById('15-seconds')?.classList.remove('active-select');
+    document.getElementById('30-seconds')?.classList.remove('active-select');
+    document.getElementById('60-seconds')?.classList.remove('active-select');
+    document.getElementById('120-seconds')?.classList.remove('active-select');
+
+    switch(words){
+      case 10: { document.getElementById('10-words')?.classList.add('active-select'); break;}
+      case 25: { document.getElementById('25-words')?.classList.add('active-select'); break;}
+      case 50: { document.getElementById('50-words')?.classList.add('active-select'); break;}
+      case 100: { document.getElementById('100-words')?.classList.add('active-select'); break;}
+    }
+
+    this.generateText(this.numWords);
+  }
+
+  setTime(time: number){
+    clearInterval(this.timerRef);
+    this.timed = true;
+    this.timeLength = time;
+    document.getElementById('10-words')?.classList.remove('active-select');
+    document.getElementById('25-words')?.classList.remove('active-select');
+    document.getElementById('50-words')?.classList.remove('active-select');
+    document.getElementById('100-words')?.classList.remove('active-select');
+
+    document.getElementById('15-seconds')?.classList.remove('active-select');
+    document.getElementById('30-seconds')?.classList.remove('active-select');
+    document.getElementById('60-seconds')?.classList.remove('active-select');
+    document.getElementById('120-seconds')?.classList.remove('active-select');
+    switch(time){
+      case 15: { document.getElementById('15-seconds')?.classList.add('active-select'); break;}
+      case 30: { document.getElementById('30-seconds')?.classList.add('active-select'); break;}
+      case 60: { document.getElementById('60-seconds')?.classList.add('active-select'); break;}
+      case 120: { document.getElementById('120-seconds')?.classList.add('active-select'); break;}
+    }
+
+    this.generateText(2*this.timeLength);
+  }
+
   generateText(length: number){
     this.timer = 0;
+    this.textString = "";
+    this.textArray = [];
     this.textString = generate(length).toString().toLowerCase().replaceAll(',', ' ');
     let tempArr: string[] = this.textString.split('');
     for(let i = 0; i < tempArr.length; i++){
@@ -28,6 +82,7 @@ export class TypingComponent implements OnInit{
     }
     this.numCharacters = this.textArray.length; 
     this.position=0;
+    document.getElementById('main-text')?.focus();
   }
   onKeyUp(event:KeyboardEvent){
     const key = event.keyCode || event.charCode; 
@@ -55,7 +110,7 @@ export class TypingComponent implements OnInit{
       if(this.position > 0) this.position--;
     }
     else{
-      // if(this.position === 0) this.startTime = Date.now();
+      if(this.position === 0) this.startTime = Date.now();
       if(this.position ===0){
         this.timerRef = setInterval(() => {
           this.timer = (Date.now() - this.startTime)/1000;
@@ -77,13 +132,31 @@ export class TypingComponent implements OnInit{
       selection?.removeAllRanges();
       selection?.addRange(range);
     }
-    if(this.position == this.numCharacters){ 
-      clearInterval(this.timerRef);
-    }
+    if((!this.timed && this.position == this.numCharacters) || (this.timed && this.timer > this.timeLength))
+      this.testComplete();
+
     return false;
-    
   }
 
+  testComplete(){
+    clearInterval(this.timerRef);
+    let typedCorrectly: number = 0;
+    let typedIncorrectly: number = 0;
+    for(let letter of this.textArray){
+      if(letter.status === 1) typedCorrectly++;
+      else if(letter.status === 2) typedIncorrectly++;
+    }
+    this.WPM = (typedCorrectly/5) / (this.timer/60);
+    this.accuracy = (typedCorrectly)/(typedIncorrectly + typedCorrectly)
+
+    document.getElementById('main-text')?.classList.remove('main-text');
+    document.getElementById('timer')?.classList.remove('timer');
+    document.getElementById('timer')?.classList.add('timer-hide');
+    document.getElementById('main-text')?.classList.add('main-text-hide');
+    document.getElementById('analysis')?.classList.remove('analysis-hide');
+    document.getElementById('analysis')?.classList.add('analysis');
+
+  }
   textClicked(){
     const element = document.getElementById('main-text');
     if (element instanceof HTMLDivElement) {
@@ -95,4 +168,18 @@ export class TypingComponent implements OnInit{
       selection?.addRange(range);
     }
   }
+  restart(){
+
+    document.getElementById('timer')?.classList.remove('timer-hide');
+    document.getElementById('main-text')?.classList.remove('main-text-hide');
+    document.getElementById('main-text')?.classList.add('main-text');
+    document.getElementById('timer')?.classList.add('timer');
+    document.getElementById('analysis')?.classList.remove('analysis');
+    document.getElementById('analysis')?.classList.add('analysis-hide');
+    document.getElementById('main-text')?.focus();
+    if(this.timed)
+      this.generateText(this.numWords);
+    else
+      this.generateText(2*this.timeLength);
+    }
 }
