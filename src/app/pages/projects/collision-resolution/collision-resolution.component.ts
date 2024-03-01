ac79b20e0155a9fbe68465005da6f8d0ebff6b3b
Victor Misyutin <misyutinvictor@gmail.com>
2024-02-29 19:56:22 -0600

    mostly finished collision resolution project

--- src/app/pages/projects/collision-resolution/collision-resolution.component.ts
+++ src/app/pages/projects/collision-resolution/collision-resolution.component.ts
@@ -9,19 +9,21 @@ export class CollisionResolutionComponent implements OnInit{
   // hashTable: { [key: string]: number } = {};
   hashTableKeys: number[] = [];
   hashTableValues: number[] = [];
-  tableSize: number = 20;
+  tableSize: number = 11;
+  insertedItems: number = 0;
   selectedCollisionTechnique: string = "linear";
   numCollisions: number = 0;
 
   ngOnInit(): void {
     const enterKeyElement = document.getElementById("tableSize") as HTMLInputElement;
-    enterKeyElement.value = '20';
+    enterKeyElement.value = '11';
     this.instantiateTable();
   }
 
   instantiateTable(){
     this.hashTableKeys = [];
     this.hashTableValues = [];
+    this.insertedItems = 0;
     for(let i = 0; i < this.tableSize; i++){
       this.hashTableKeys[i] = 0;
       this.hashTableValues[i] = 0;
@@ -29,25 +31,84 @@ export class CollisionResolutionComponent implements OnInit{
   }
   onChangeCollisionTechnique(event: any) {
     this.selectedCollisionTechnique = event.target.value;
+    this.instantiateTable();
   }
+
+  isPrime(num: number): boolean {
+    if (num <= 1) {
+      return false;
+    }
+    if (num <= 3) {
+      return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+      return false;
+    }
+    let i = 5;
+    while (i * i <= num) {
+      if (num % i === 0 || num % (i + 2) === 0) {
+        return false;
+      }
+      i += 6;
+    }
+    return true;
+  }
+
+  nearestPrimeLessThan(value: number): number {
+    let prime = value - 1;
+    while (prime > 1) {
+      if (this.isPrime(prime)) {
+        return prime;
+      }
+      prime--;
+    }
+    return -1;
+  }
+
+
+  hash2(key: number){
+    let doubleFactor: number = (this.nearestPrimeLessThan(this.tableSize));
+    return (doubleFactor - (key%doubleFactor));
+  }
+
   addKey(key: number){
+    if(this.insertedItems >= this.tableSize) return;
     let index: number = 0;
     index = key % this.tableSize;
     if(this.hashTableKeys[index] !== 0){
-      this.numCollisions++;
       if(this.selectedCollisionTechnique === "linear"){
         // linear probing
+        while(this.hashTableKeys[index] !== 0){
+          this.numCollisions++;
+          index = (index + 1) % this.tableSize;
+        }
+        this.hashTableKeys[index] = key;
       }
       else if(this.selectedCollisionTechnique === "quadratic"){
         // quadratic probing 
-        
+        let startingIndex = index;
+        let count: number = 1;
+        while(this.hashTableKeys[index] !== 0){
+          this.numCollisions++;
+          index = (startingIndex + count*count) % this.tableSize;
+          count++;
+        }
+        this.hashTableKeys[index] = key;
       }
       else{
         // double hash
-        
+        let startingIndex = index;
+        let count: number = 1;
+        while(this.hashTableKeys[index] !== 0){
+          this.numCollisions++;
+          index = (startingIndex + count * this.hash2(key)) % this.tableSize;
+          count++;
+        }
+        this.hashTableKeys[index] = key;
       }
     }
     this.hashTableKeys[index] = key;
+    this.insertedItems++;
   }
   
   enterKey() {
@@ -66,9 +127,11 @@ export class CollisionResolutionComponent implements OnInit{
   
   changeTableSize(event: Event) {
     const target = event.target as HTMLInputElement;
-    let num: number = 20;
+    let num: number = 11;
     if(target.value != '') num = parseInt(target.value);   
     if(num > 100) num = 100;
+    else if(num < 5) num = 5;
+
     this.tableSize = num;
     this.instantiateTable();
   }
