0ec55d3f2c875ceb0e71a5ba908de0d84e5b256c
Victor Misyutin <misyutinvictor@gmail.com>
2024-03-12 18:04:54 -0500

    changes to contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -27,14 +27,12 @@ export class ContactComponent implements OnInit{
       // do something with the values
 
 
-      document.getElementById('post-submission-text')?.classList.remove('sub-text-hidden');
       document.getElementById('post-submission-text')?.classList.remove('sub-text-invalid');
       document.getElementById('post-submission-text')?.classList.add('sub-text-valid');
       this.postSubmissionText = "Thank you for your submission!";
       this.contactForm.reset();
       // Proceed with form submission logic
     } else {
-      document.getElementById('post-submission-text')?.classList.remove('sub-text-hidden');
       document.getElementById('post-submission-text')?.classList.add('sub-text-invalid');
       this.postSubmissionText = "Something went wrong, please double check the form.";
     }
