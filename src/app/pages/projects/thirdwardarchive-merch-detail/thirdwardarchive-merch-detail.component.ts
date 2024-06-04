4825b9754a23aa5d96567f58b2d677277d78e1ea
Victor Misyutin <misyutinvictor@gmail.com>
2024-06-04 03:57:16 -0500

    image api woeking !!! woohooo

--- src/app/pages/projects/thirdwardarchive-merch-detail/thirdwardarchive-merch-detail.component.ts
+++ src/app/pages/projects/thirdwardarchive-merch-detail/thirdwardarchive-merch-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-thirdwardarchive-merch-detail',
   templateUrl: './thirdwardarchive-merch-detail.component.html',
-  styleUrl: './thirdwardarchive-merch-detail.component.css'
+  styleUrls: ['./thirdwardarchive-merch-detail.component.css']
 })
-export class ThirdwardarchiveMerchDetailComponent {
+export class ThirdwardarchiveMerchDetailComponent implements OnInit {
+  imageUrl: string | undefined;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchImage();
+  }
+
+  fetchImage() {
+    this.http.get('https://vicmis.com/api/get-file', { responseType: 'blob' }).subscribe(
+      (response) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          this.imageUrl = reader.result as string;
+        };
+        reader.readAsDataURL(response);
+      },
+      (error) => {
+        console.error('Error fetching image', error);
+      }
+    );
+  }
 }
