03f2675bb20dc39218ca933c91e2fdb3a8d52565
Victor Misyutin <misyutinvictor@gmail.com>
2024-06-06 14:29:37 -0600

    change css for hidden login and change css for admin products view

--- src/app/pages/projects/thirdwardarchive-admin-products-details/thirdwardarchive-admin-products-details.component.ts
+++ src/app/pages/projects/thirdwardarchive-admin-products-details/thirdwardarchive-admin-products-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-thirdwardarchive-admin-products-details',
   templateUrl: './thirdwardarchive-admin-products-details.component.html',
   styleUrl: './thirdwardarchive-admin-products-details.component.css'
 })
-export class ThirdwardarchiveAdminProductsDetailsComponent {
-  productLines: string[] = [];
+export class ThirdwardarchiveAdminProductsDetailsComponent implements OnInit{
+  productLines: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData() {
+    console.log('here')
+    this.http.get(`https://vicmis.com/api/get-all-products-info/`).subscribe(
+      (response) => {
+        this.productLines = response;
+      },
+      (error) => {
+        console.error('Error fetching data', error);
+      }
+    );
+  }
+
 }
