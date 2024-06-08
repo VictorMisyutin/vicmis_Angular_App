828b1615cd18f0427dd19bf8e7595e03490840de
Victor Misyutin <misyutinvictor@gmail.com>
2024-06-07 22:02:24 -0700

    Working on checkout page

--- src/app/pages/projects/thirdwardarchive-checkout/thirdwardarchive-checkout.component.ts
+++ src/app/pages/projects/thirdwardarchive-checkout/thirdwardarchive-checkout.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { local } from 'd3';
 
 @Component({
   selector: 'app-thirdwardarchive-checkout',
   templateUrl: './thirdwardarchive-checkout.component.html',
   styleUrl: './thirdwardarchive-checkout.component.css'
 })
-export class ThirdwardarchiveCheckoutComponent {
+export class ThirdwardarchiveCheckoutComponent implements OnInit{
+
+  cart_ids: string[] = [];
+
+  ngOnInit(): void {
+    const storedCart = localStorage.getItem("cart");
+    if (storedCart) {
+      this.cart_ids = JSON.parse(storedCart);
+    }
+  }
 
 }
