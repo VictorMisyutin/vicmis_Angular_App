52afae2da98b0c131deb0b401371451b0856d4fe
Victor Misyutin <misyutinvictor@gmail.com>
2024-08-23 15:26:58 -0500

    Started working on another blog post, change CSS organization for posts, and remove old comments.

--- src/app/pages/blog/posts/an-introduction-to-vim/an-introduction-to-vim.component.ts
+++ src/app/pages/blog/posts/an-introduction-to-vim/an-introduction-to-vim.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-an-introduction-to-vim',
   templateUrl: './an-introduction-to-vim.component.html',
-  styleUrl: './an-introduction-to-vim.component.css'
+  styleUrls: ['./an-introduction-to-vim.component.css', '../posts.css']
 })
 export class AnIntroductionToVimComponent {
 
--- src/app/pages/blog/posts/my-experience-with-linux-and-windows/my-experience-with-linux-and-windows.component.ts
+++ src/app/pages/blog/posts/my-experience-with-linux-and-windows/my-experience-with-linux-and-windows.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-experience-with-linux-and-windows',
   templateUrl: './my-experience-with-linux-and-windows.component.html',
-  styleUrl: './my-experience-with-linux-and-windows.component.css'
+  styleUrls: ['./my-experience-with-linux-and-windows.component.css', '../posts.css']
 })
 export class MyExperienceWithLinuxAndWindowsComponent {
 
